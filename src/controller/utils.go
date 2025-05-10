5b1cc9e60eb67f8d141a439054af934d40375774
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-05-10 19:21:49 -0300

    feat: controller utils Get auth user Validate date to "YYYY-MM-DD" format

---
+++ src/controller/utils.go
@@ -0,0 +1,44 @@
+package controller
+
+import (
+	"time"
+
+	"github.com/NutriPocket/UserService/model"
+	"github.com/gin-gonic/gin"
+)
+
+var authError = &model.AuthenticationError{
+	Title:  "Unauthorized user",
+	Detail: "The user isn't authorized to access this endpoint",
+}
+
+func GetAuthUser(c *gin.Context) (*model.User, error) {
+	var user *model.User
+	var ok bool
+
+	authUser, exists := c.Get("authUser")
+	if !exists {
+		return nil, authError
+	}
+
+	if user, ok = authUser.(*model.User); !ok {
+		return nil, authError
+	}
+
+	if userId := c.Param("userId"); userId != user.ID {
+		return nil, authError
+	}
+
+	return user, nil
+}
+
+func ValidateDate(date string) error {
+	if _, err := time.Parse("2006-01-02", date); err != nil {
+		return &model.ValidationError{
+			Title:  "Invalid date",
+			Detail: "The format of the date is invalid, expected format: YYYY-MM-DD",
+		}
+	}
+
+	return nil
+}
